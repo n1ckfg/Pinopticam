d3b0a0a8785576264ef23d313b83876eb34b4d6d
Nick Fox-Gieg <nick@fox-gieg.com>
2020-08-17 03:06:04 -0400

    shell scripts

--- setup.sh
+++ setup.sh
@@ -3,13 +3,14 @@
 DIR=$PWD
 
 cd ../../../addons
+git clone https://github.com/n1ckfg/ofxCvPiCam
+#git clone https://github.com/n1ckfg/ofxOMXCamera
 git clone https://github.com/n1ckfg/ofxHTTP
 git clone https://github.com/n1ckfg/ofxIO
 git clone https://github.com/n1ckfg/ofxMediaType
 git clone https://github.com/n1ckfg/ofxNetworkUtils
 git clone https://github.com/n1ckfg/ofxSSLManager
 git clone https://github.com/n1ckfg/ofxSSLManager
-git clone https://github.com/n1ckfg/ofxOMXCamera
 git clone https://github.com/n1ckfg/ofxJSON
 
 cd $DIR
