d5b12bf5f0ffc9ef8ad381d9065015a5608c5f54
Nick Fox-Gieg <nick@fox-gieg.com>
2023-08-21 15:24:46 -0400

    test

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,5 +1,9 @@
 #include "ofApp.h"
 
+#include "../../common/src/Pinopticon.hpp"
+#include "../../common/src/Pinopticon_Osc.hpp"
+#include "../../common/src/Pinopticon_Http.hpp"
+
 using namespace cv;
 using namespace ofxCv;
 using namespace Pinopticon;
